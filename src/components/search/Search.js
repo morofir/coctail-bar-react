d157714b72bfa62a331b5ff6a01a5d4c7798f63d
morofir <39213394+morofir@users.noreply.github.com>
2021-07-16 09:47:14 +0300

    Add files via upload

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,56 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 import axios from "axios";
-import Item from "./Item";
 
 const INITIAL_STATE = {
   term: "",
 };
-let a;
+
 const Search = () => {
   const [values, setValues] = useState(INITIAL_STATE);
-  const [responseData, setresponseData] = useState({});
 
   const handleChange = (event) => {
-    const { name, value } = event.target;
-    setValues((prevState) => ({ ...prevState, [name]: value }));
+    const [name, value] = event.target;
+    setValues((presvState) => ({ ...presvState, [name]: value }));
   };
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(values.term);
-
-    runSearch(values.term);
-  };
-  const runSearch = (term) => {
-    axios
-      .get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${term}`)
-      .then((response) => {
-        setresponseData(response.data); //get data from api key
-      })
-      .catch((error) => {
-        console.log("error:", error); // catch error
-      })
-      .finally(() => {
-        setValues(INITIAL_STATE); //clean the term
-      });
-  };
-
   return (
-    <>
-      <form onSubmit={handleSubmit} className="search">
-        <input
-          onChange={handleChange}
-          type="text"
-          name="term"
-          className="search-input"
-          placeholder="Search..."
-          value={values.term}
-        />
-      </form>
-      {responseData.drinks &&
-        responseData.drinks
-          .slice(0, 7)
-          .map((item) => <Item key={item.idDrink} item={item} />)}
-    </>
+    <form>
+      <input
+        type="text"
+        name="term"
+        className="search-input"
+        onChange={handleChange}
+        placeholder="Search..."
+        value={values.term}
+      />
+    </form>
   );
 };
+
 export default Search;
